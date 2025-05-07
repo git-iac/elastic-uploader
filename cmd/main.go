c621858870593669868577b648a81bbc5c4f9977
Vitalii Kohut <vkohut@intelliarts.com>
2025-05-07 10:06:40 +0300

    fix: use passed read chunk size

--- cmd/main.go
+++ cmd/main.go
@@ -10,8 +10,6 @@ import (
 	"github.com/git-iac/elastic-uploader/pkg"
 )
 
-const readChunkSize = 1000
-
 func main() {
 	pkg.WithMetrics(func() {
 		opts := cli.Parse()
@@ -30,15 +28,14 @@ func main() {
 		switch opts.IndexAction {
 		case cli.PopulateIndex:
 			log.Printf("populating index [%s]...", pkg.SectionIndex)
-			if err := cltService.UploadBulk(ctx, readChunkSize); err != nil {
+			if err := cltService.UploadBulk(ctx, opts.ReadChunkSize); err != nil {
 				//TODO: errors with special chars look ugly
 				log.Fatalf("error populating index: %s", err)
 			}
-
 		case cli.CreateIndex:
 			log.Printf("creating index [%s]...", pkg.SectionIndex)
 			if err := cltService.CreateIndexForSections(ctx, pkg.IndexBody, pkg.SectionIndex); err != nil {
-				log.Fatalf("error populating index: %s", err)
+				log.Fatalf("error creating index: %s", err)
 			}
 		default:
 			log.Fatal("no action flag was passed")
